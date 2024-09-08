f73b117f0749edd4d69459692091e4628538181c
hakim <morasiregar360@gmail.com>
2024-09-08 22:13:59 +0700

    add error handling for image and fixing playing video in backgroung active

--- Views/Technique/GifImage.swift
+++ Views/Technique/GifImage.swift
@@ -17,24 +17,32 @@ struct GifImage: UIViewRepresentable {
 
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
-        let url = Bundle.main.url(forResource: name, withExtension: "gif")!
-        let data = try! Data(contentsOf: url)
-        webView.load(
-            data,
-            mimeType: "image/gif",
-            characterEncodingName: "UTF-8",
-            baseURL: url.deletingLastPathComponent()
-        )
-        webView.scrollView.isScrollEnabled = false
+        guard let url = Bundle.main.url(forResource: name, withExtension: "gif") else {
+            print("Error: GIF file not found")
+            return webView
+        }
 
+        do {
+            let data = try Data(contentsOf: url)
+            webView.load(
+                data,
+                mimeType: "image/gif",
+                characterEncodingName: "UTF-8",
+                baseURL: url.deletingLastPathComponent()
+            )
+        } catch {
+            print("Error loading GIF: \(error)")
+        }
+
+        webView.scrollView.isScrollEnabled = false
         return webView
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
         uiView.reload()
     }
-
 }
+
 
 
 struct GifImage_Previews: PreviewProvider {
